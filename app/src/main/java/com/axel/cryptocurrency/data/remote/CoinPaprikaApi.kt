e80b6ea7fcdfc7ea6f6cb9256747a005d87489b9
axelessono <naxelcdric@gmail.com>
2023-10-08 03:25:28 +0200

    Created DTO according to json format

---
+++ app/src/main/java/com/axel/cryptocurrency/data/remote/CoinPaprikaApi.kt
@@ -0,0 +1,16 @@
+package com.axel.cryptocurrency.data.remote
+
+import com.axel.cryptocurrency.data.remote.dto.CoinDetailDto
+import com.axel.cryptocurrency.data.remote.dto.CoinDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CoinPaprikaApi {
+
+    @GET("/v1/coins")
+    suspend fun getCoins() : List<CoinDto>
+
+    @GET("/v1/coins/{coinId}")
+    suspend fun getCoinById(@Path("coinId") coinId : String) : CoinDetailDto
+
+}
