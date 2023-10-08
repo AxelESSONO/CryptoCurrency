a436377706bf4788dcfa6e2e65a7f5a3835c9a1c
axelessono <naxelcdric@gmail.com>
2023-10-08 06:51:06 +0200

    Created components

---
+++ app/src/main/java/com/axel/cryptocurrency/presentation/Screen.kt
@@ -0,0 +1,8 @@
+package com.axel.cryptocurrency.presentation
+
+sealed class Screen(val route :  String){
+
+    object CoinListScreen : Screen(route = "coin_list_screen")
+    object CoinDetailScreen : Screen(route = "coin_detail_screen")
+
+}
